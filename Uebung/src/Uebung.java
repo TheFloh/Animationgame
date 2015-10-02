3f26966dd9525b9357d0107c99d19376ba599180
Huber.Florian <Huber.Florian@SK-D8CB8A4FAAFF>
2015-10-02 08:43:00 +0200

    Übung zu Bedingung

--- Uebung/src/Uebung.java
+++ Uebung/src/Uebung.java
@@ -4,6 +4,15 @@ public class Uebung {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+	
+			 
+			 System.out.println(
+			 (17+88)*2==42?"tatsaechlich gleich":"unterschiedlich");
+			
+			 }
+			
+	
+	
 	}
 
-}
+
